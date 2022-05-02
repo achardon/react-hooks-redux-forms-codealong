c3ca8b7fe8da2242c5c75aa4dfebe7f8204acf2a
Alessandra Chardon <achardon@ucla.edu>
2022-05-02 10:41:21 -0400

    assignment complete

--- src/features/todos/CreateTodo.js
+++ src/features/todos/CreateTodo.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
+import { useDispatch } from "react-redux";
+import { todoAdded } from "./todosSlice"
 
 function CreateTodo() {
-  return <div>Create Todo Component</div>;
+  
+  const [todo, setTodo] = useState("")
+  const dispatch = useDispatch()
+  
+  function handleChange(e) {
+    setTodo(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    dispatch(todoAdded(todo))
+  }
+
+  return <div>
+    <form onSubmit={handleSubmit}>
+      <p>
+        <label>add todo</label>
+        <input type="text" value={todo} onChange={handleChange} />
+      </p>
+      <input type="submit" />
+    </form>
+  </div>;
 }
 
 export default CreateTodo;
